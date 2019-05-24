3842892ae567b53cb6e9517fc51469442485b24e
Ian Zimmerman <itz@no-use.mooo.com>
2019-05-24 09:48:28 -0700

    Add pair and triple functions to Lift

--- src/Lift.ml
+++ src/Lift.ml
@@ -16,3 +16,14 @@ let fold g f a x =
   | Ok a', _ ->
      let f' x' = f a' x' in
      x |> g |>> f'
+
+let pair = function
+  | (Error _ as e), _ -> e
+  | _, (Error _ as e) -> e
+  | Ok x, Ok y -> Ok (x, y)
+
+let triple = function
+  | (Error _ as e), _, _ -> e
+  | _, (Error _ as e), _ -> e
+  | _, _, (Error _ as e) -> e
+  | Ok x, Ok y, Ok z -> Ok (x, y, z)
--- src/Lift.mli
+++ src/Lift.mli
@@ -5,3 +5,9 @@ val (|>>|) : ('a, 'c) result -> ('a -> ('b, 'c) result) -> ('b, 'c) result
 val fold :
   ('a -> ('b, 'c) result) ->
   ('d -> 'b -> 'e) -> ('d, 'c) result -> 'a -> ('e, 'c) result
+
+val pair : ('a, 'c) result * ('b, 'c) result -> ('a * 'b, 'c) result
+
+val triple :
+  ('a, 'c) result * ('b, 'c) result * ('d, 'c) result ->
+  ('a * 'b * 'd, 'c) result
