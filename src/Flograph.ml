8c782f9df0976d07f4fe425a1dd7042b85193560
Ian Zimmerman <itz@no-use.mooo.com>
2019-06-28 23:17:24 -0700

    Add a dot engine module, needs beauty pass

--- src/Flograph.ml
+++ src/Flograph.ml
@@ -17,8 +17,14 @@ module Dfs =
 
 let has_cycle g = Dfs.has_cycle g
 
-exception Consistency of string list
-                 
+exception Inconsistent of string list
+
+let raise_hotter n1 n2 =
+  raise (Inconsistent [n1; "is hotter than its dependency"; n2])
+
+let raise_blocked n1 n2 =
+  raise (Inconsistent [n1; "is blocked by"; n2])
+
 let check_edge v1 v2 =
   let n1 = Unlinked.name v1 in
   let n2 = Unlinked.name v2 in
@@ -26,12 +32,39 @@ let check_edge v1 v2 =
   let s2 = Unlinked.state v2 in
   let t1 = Unlinked.temperature v1 in
   let t2 = Unlinked.temperature v2 in
-  if t1 < t2 then raise (Consistency [n1; "is hotter than its dependency"; n2])
+  if Temperature.compare t1 t2 < 0 then raise_hotter n1 n2
   else match s1, s2 with
        | _, State.Blocked -> ()
        | State.Done, _ -> ()
-       | _, _ -> raise (Consistency [n1; "is blocked by"; n2])
+       | _, _ -> raise_blocked n1 n2
 
 let check_consistency g =
   try M.iter_edges check_edge g ; return g with
-  | Consistency l -> Resultx.error (String.concat " " l)
+  | Inconsistent l -> Resultx.error (String.concat " " l)
+
+module Dot_params =
+  struct
+
+  type t = M.t
+  module V = M.V
+  module E = M.E
+  let iter_vertex = M.iter_vertex
+  let iter_edges_e = M.iter_edges_e
+
+  let graph_attributes _ = []
+  let default_vertex_attributes _ = []
+  let vertex_name v = Unlinked.name v
+  let get_subgraph _ = None
+  let default_edge_attributes _ = []
+  let edge_attributes _ = []
+
+  let vertex_attributes v =
+    match Unlinked.state v with
+    | State.Blocked -> [`Shape `Parallelogram; `Fillcolor 0xf4bada]
+    | State.Done -> [`Shape `Oval; `Fillcolor 0xeeeeee]
+    | State.Ready -> [`Shape `Oval; `Fillcolor 0xffffff]
+    | State.Next -> [`Shape `Oval; `Fillcolor 0x00cc00]
+
+  end
+
+module Dot_engine = Graph.Graphviz.Dot (Dot_params)
